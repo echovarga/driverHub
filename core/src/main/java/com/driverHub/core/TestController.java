53c509e32b61462ff34d3bf3a5d18cd101b302b6
echovarga <echovarga@gmail.com>
2022-07-18 19:51:31 +0300

    Dicker files

---
+++ core/src/main/java/com/driverHub/core/TestController.java
@@ -0,0 +1,12 @@
+package com.driverHub.core;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/messages")
+    public String getMessage() {
+        return "Hello from Docker!";
+    }
+}
